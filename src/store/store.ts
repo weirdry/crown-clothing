4870f6f4dd75dab77a147ac1c881fc494c9d4a6a
ED <granrr.z@gmail.com>
2022-09-24 19:24:00 +0900

    Redux store migration to typescript done

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,21 +2,32 @@ import {
 	compose,
 	legacy_createStore as createStore,
 	applyMiddleware,
+	Middleware,
 } from 'redux'
-import { persistStore, persistReducer } from 'redux-persist'
+import { persistStore, persistReducer, PersistConfig } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 import logger from 'redux-logger'
-// import thunk from 'redux-thunk'
 import createSagaMiddleware from 'redux-saga'
 
 import { rootSaga } from './root-saga'
 
 import { rootReducer } from './root-reducer'
 
-const persistConfig = {
+export type RootState = ReturnType<typeof rootReducer>
+
+declare global {
+	interface Window {
+		__REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose
+	}
+}
+
+type ExtendedPersistConfig = PersistConfig<RootState> & {
+	whitelist: (keyof RootState)[]
+}
+
+const persistConfig: ExtendedPersistConfig = {
 	key: 'root',
 	storage: storage,
-	// blacklist: ['user'],
 	whitelist: ['cart'],
 }
 
@@ -27,8 +38,7 @@ const sagaMiddleware = createSagaMiddleware()
 const middlewares = [
 	process.env.NODE_ENV !== 'production' && logger,
 	sagaMiddleware,
-	// thunk,
-].filter(Boolean)
+].filter((middleware): middleware is Middleware => Boolean(middleware))
 
 const composedEnhancer =
 	(process.env.NODE_ENV !== 'production' &&
